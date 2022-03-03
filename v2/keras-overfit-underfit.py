87a85d544d0aa85ec61f8156faea9e5061858045
menglimeng <dream_15320@163.com>
2022-03-03 17:58:37 +0800

    update keras

--- v2/keras-overfit-underfit.py
+++ v2/keras-overfit-underfit.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Mar  3 14:36:22 2022
+  * large csv data. tf.data.experimental.CsvDataset
+  * reduce the learning rate over time
+  * callbacks(tfdocs.EpochDots, EarlyStopping, TensorBoard)
+  * tfdocs.plots
+  * L2 + dropout
+
+Most common ways to prevent overfitting in neural networks:
+
+  * Get more training data.
+  * Reduce the capacity of the network.
+  * Add weight regularization.
+  * Add dropout.
+  * Data augmentation
+  * Batch normalization (tf.keras.layers.BatchNormalization)
 
 @author: DELL
 """
@@ -27,3 +41,208 @@ import tempfile
 
 logdir = pathlib.Path(tempfile.mkdtemp())/"tensorboard_logs"
 shutil.rmtree(logdir, ignore_errors=True)
+
+
+# gz = tf.keras.utils.get_file('HIGGS.csv.gz', 'http://mlphysics.ics.uci.edu/data/higgs/HIGGS.csv.gz')
+
+gz = r'D:\work\data\训练数据\通用\HIGGS.csv.gz'
+
+FEATURES = 28
+ds = tf.data.experimental.CsvDataset(gz,[float(),]*(FEATURES+1), compression_type="GZIP")
+
+# repacks that list of scalars into a (feature_vector, label) pair
+def pack_row(*row):
+  label = row[0]
+  features = tf.stack(row[1:],1)
+  return features, label
+
+# make a new tf.data.Dataset that takes batches of 10,000 examples, applies the pack_row function to each batch, and then splits the batches back up into individual records
+packed_ds = ds.batch(10000).map(pack_row).unbatch()
+# 
+for features,label in packed_ds.batch(1000).take(1):
+  # print(features[0])
+  plt.hist(features.numpy().flatten(), bins = 101)
+
+# use just the first 1,000 samples for validation, and the next 10,000 for training
+N_VALIDATION = int(1e3)
+N_TRAIN = int(1e4)
+BUFFER_SIZE = int(1e4)
+BATCH_SIZE = 500
+STEPS_PER_EPOCH = N_TRAIN//BATCH_SIZE
+
+# use the Dataset.cache method to ensure that the loader doesn't need to re-read the data from the file on each epoch
+validate_ds = packed_ds.take(N_VALIDATION).cache()
+train_ds = packed_ds.skip(N_VALIDATION).take(N_TRAIN).cache()
+## use Dataset.shuffle and Dataset.repeat on the training set
+validate_ds = validate_ds.batch(BATCH_SIZE)
+train_ds = train_ds.shuffle(BUFFER_SIZE).repeat().batch(BATCH_SIZE)
+
+# Start with a simple model using only densely-connected layers (tf.keras.layers.Dense) as a baseline, then create larger models, and compare them
+
+# reduce the learning rate during training
+lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+  0.001,
+  decay_steps=STEPS_PER_EPOCH*1000,
+  decay_rate=1,
+  staircase=False)
+
+def get_optimizer():
+  return tf.keras.optimizers.Adam(lr_schedule)
+
+# hyperbolically decrease the learning rate to 1/2 of the base rate at 1,000 epochs, 1/3 at 2,000 epochs, and so on
+step = np.linspace(0,100000)
+lr = lr_schedule(step)
+plt.figure(figsize = (8,6))
+plt.plot(step/STEPS_PER_EPOCH, lr)
+plt.ylim([0,max(plt.ylim())])
+plt.xlabel('Epoch')
+_ = plt.ylabel('Learning Rate')
+
+# use the tfdocs.EpochDots which simply prints a . for each epoch, and a full set of metrics every 100 epochs
+# include tf.keras.callbacks.EarlyStopping to avoid long and unnecessary training times
+# Use callbacks.TensorBoard to generate TensorBoard logs for the training
+def get_callbacks(name):
+  return [
+    tfdocs.modeling.EpochDots(),
+    tf.keras.callbacks.EarlyStopping(monitor='val_binary_crossentropy', patience=200),
+    tf.keras.callbacks.TensorBoard(logdir/name),
+  ]
+# each model will use the same Model.compile and Model.fit settings
+def compile_and_fit(model, name, optimizer=None, max_epochs=10000):
+  if optimizer is None:
+    optimizer = get_optimizer()
+  model.compile(optimizer=optimizer,
+                loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                metrics=[
+                  tf.keras.losses.BinaryCrossentropy(
+                      from_logits=True, name='binary_crossentropy'),
+                  'accuracy'])
+
+  model.summary()
+
+  history = model.fit(
+    train_ds,
+    steps_per_epoch = STEPS_PER_EPOCH,
+    epochs=max_epochs,
+    validation_data=validate_ds,
+    callbacks=get_callbacks(name),
+    verbose=0)
+  return history
+
+# Tiny model
+tiny_model = tf.keras.Sequential([
+    layers.Dense(16, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(1)
+])
+
+size_histories = {}
+
+size_histories['Tiny'] = compile_and_fit(tiny_model, 'sizes/Tiny')
+
+plotter = tfdocs.plots.HistoryPlotter(metric = 'binary_crossentropy', smoothing_std=10)
+plotter.plot(size_histories)
+plt.ylim([0.5, 0.7])
+
+# Small model
+small_model = tf.keras.Sequential([
+    # `input_shape` is only required here so that `.summary` works.
+    layers.Dense(16, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(16, activation='elu'),
+    layers.Dense(1)
+])
+
+size_histories['Small'] = compile_and_fit(small_model, 'sizes/Small')
+
+# Medium model
+medium_model = tf.keras.Sequential([
+    layers.Dense(64, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(64, activation='elu'),
+    layers.Dense(64, activation='elu'),
+    layers.Dense(1)
+])
+size_histories['Medium']  = compile_and_fit(medium_model, "sizes/Medium")
+
+# Large model
+large_model = tf.keras.Sequential([
+    layers.Dense(512, activation='elu', input_shape=(FEATURES,)),
+    layers.Dense(512, activation='elu'),
+    layers.Dense(512, activation='elu'),
+    layers.Dense(512, activation='elu'),
+    layers.Dense(1)
+])
+size_histories['large'] = compile_and_fit(large_model, "sizes/large")
+
+# Plot the training and validation losses
+plotter.plot(size_histories)
+a = plt.xscale('log')
+plt.xlim([5, max(plt.xlim())])
+plt.ylim([0.5, 0.7])
+plt.xlabel("Epochs [Log Scale]")
+
+# View in TensorBoard
+# tensorboard --logdir {logdir}/sizes
+
+
+############ Strategies to prevent overfitting
+shutil.rmtree(logdir/'regularizers/Tiny', ignore_errors=True)
+shutil.copytree(logdir/'sizes/Tiny', logdir/'regularizers/Tiny')
+
+regularizer_histories = {}
+regularizer_histories['Tiny'] = size_histories['Tiny']
+
+# L2 regularization
+l2_model = tf.keras.Sequential([
+    layers.Dense(512, activation='elu',
+                 kernel_regularizer=regularizers.l2(0.001),
+                 input_shape=(FEATURES,)),
+    layers.Dense(512, activation='elu',
+                 kernel_regularizer=regularizers.l2(0.001)),
+    layers.Dense(512, activation='elu',
+                 kernel_regularizer=regularizers.l2(0.001)),
+    layers.Dense(512, activation='elu',
+                 kernel_regularizer=regularizers.l2(0.001)),
+    layers.Dense(1)
+])
+
+regularizer_histories['l2'] = compile_and_fit(l2_model, "regularizers/l2")
+
+# Add dropout
+dropout_model = tf.keras.Sequential([
+    layers.Dense(512, activation='elu', input_shape=(FEATURES,)),
+    layers.Dropout(0.5),
+    layers.Dense(512, activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(512, activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(512, activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(1)
+])
+
+regularizer_histories['dropout'] = compile_and_fit(dropout_model, "regularizers/dropout")
+
+# Combined L2 + dropout
+combined_model = tf.keras.Sequential([
+    layers.Dense(512, kernel_regularizer=regularizers.l2(0.0001),
+                 activation='elu', input_shape=(FEATURES,)),
+    layers.Dropout(0.5),
+    layers.Dense(512, kernel_regularizer=regularizers.l2(0.0001),
+                 activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(512, kernel_regularizer=regularizers.l2(0.0001),
+                 activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(512, kernel_regularizer=regularizers.l2(0.0001),
+                 activation='elu'),
+    layers.Dropout(0.5),
+    layers.Dense(1)
+])
+
+regularizer_histories['combined'] = compile_and_fit(combined_model, "regularizers/combined")
+
+# plot
+plotter.plot(regularizer_histories)
+plt.ylim([0.5, 0.7])
+
+# tensorboard
+# tensorboard --logdir {logdir}/regularizers
